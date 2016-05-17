ebdea31a6cfcef236349a85b7c5c436ecb5ecf8e
sneusse <s.neusser@gmail.com>
2016-05-17 10:51:13 +0200

    initial commit

--- src/DotOverload.jl
+++ src/DotOverload.jl
@@ -1,5 +1,132 @@
 module DotOverload
 
-# package code goes here
+
+export getMember
+export setMember!
+export @dotted
+
+macro dotted(ex::Expr)
+
+  function getExpression(ex, mem)
+    if (mem != nothing)
+      return Expr(:call, :getMember, ex, mem)
+    end
+    return ex
+  end
+
+  function treewalk(stuff)
+    # return whatever is not an expression
+    if (!isa(stuff, Expr))
+      return stuff, nothing
+    end
+
+    # care only about binary expressions ( dot, assignment )
+    if (length(stuff.args) == 2)
+      h = stuff.head
+
+      left, leftmember = nothing, nothing
+
+      # special case (should be ignored):
+      # module.func(p) = 2*p
+      if (isa(stuff.args[1], Expr)
+        && isa(stuff.args[2], Expr)
+        && h == :(=)
+        && stuff.args[1].head == :call
+        && stuff.args[2].head == :block)
+
+        # ignore for now
+        left, leftmember = stuff.args[1], nothing
+      else
+        left, leftmember = treewalk(stuff.args[1])
+      end
+
+      right, rightmember = treewalk(stuff.args[2])
+
+      getleft = getExpression(left, leftmember)
+      right = getExpression(right, rightmember)
+
+      # that's what we're looking for
+      if ( h == :(.) )
+        return getleft, right
+      end
+
+      # look for assignment
+      if (leftmember != nothing)
+        setleft = nothing
+
+        if ( h == :(=) )
+          setleft = right
+        elseif ( h == :(+=) )
+          setleft = :($getleft + $right)
+        elseif ( h == :(-=) )
+          setleft = :($getleft - $right)
+        elseif ( h == :(*=) )
+          setleft = :($getleft * $right)
+        elseif ( h == :(/=) )
+          setleft = :($getleft / $right)
+        elseif ( h == :(\=) )
+          setleft = :($getleft \ $right)
+        elseif ( h == :(÷=) )
+          setleft = :($getleft ÷ $right)
+        elseif ( h == :(%=) )
+          setleft = :($getleft % $right)
+        elseif ( h == :(^=) )
+          setleft = :($getleft ^ $right)
+        elseif ( h == :(&=) )
+          setleft = :($getleft & $right)
+        elseif ( h == :(|=) )
+          setleft = :($getleft | $right)
+        elseif ( h == :($=) )
+          setleft = :($getleft $ $right)
+        elseif ( h == :(>>>=) )
+          setleft = :($getleft >>> $right)
+        elseif ( h == :(>>=) )
+          setleft = :($getleft >> $right)
+        elseif ( h == :(<<=) )
+          setleft = :($getleft << $right)
+        end
+
+        # If it is an assignment expression, return to parent expression
+        # and discard the assignment. Instead generate a call expression.
+        if (setleft != nothing)
+          return Expr(:call, :setMember!, left, leftmember, setleft), nothing
+        end
+      end
+
+      # if we did not have an assignment or a dot expression
+      # we will end here.
+      stuff.args[1] = getleft
+      stuff.args[2] = right
+
+      return stuff, nothing
+    end
+
+    # non binary expressions -> fix all args
+    for i = 1:length(stuff.args)
+      ex, mem = treewalk(stuff.args[i])
+      stuff.args[i] = getExpression(ex, mem)
+    end
+
+    return stuff, nothing
+  end
+
+  ex, mem = treewalk(ex)
+  ex = getExpression(ex, mem)
+
+  if (ex.head == :module)
+    return Expr(:escape, Expr(:toplevel, ex))
+  end
+
+  return Expr(:escape, ex)
+end
+
+function getMember(t, m)
+  return Base.getfield(t, m)
+end
+
+function setMember!(t, m, v)
+  return Base.setfield!(t,m,v)
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,154 @@
 using DotOverload
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+type TT
+  F::Int64
+end
+
+# basic assertions
+
+@test macroexpand( :( @dotted a.f )) == :(getMember(a,:f))
+@test macroexpand( :( @dotted a.b.c = b.g += e.f )) == :(setMember!(getMember(a,:b),:c,setMember!(b,:g,getMember(b,:g) + getMember(e,:f))))
+@test macroexpand( :( @dotted (a).b )) == :(getMember(a,:b))
+
+# there are some parsing changes between 0.4 - 0.5?
+if (VERSION >= v"0.5.0-dev")
+  @test macroexpand( :( @dotted a.(b) )) == :(getMember(a,(b,)))
+  @test macroexpand( :( @dotted anything.(10) )) == :(getMember(anything,(10,)))
+else
+  @test macroexpand( :( @dotted a.(b) )) == :(getMember(a,b))
+  @test macroexpand( :( @dotted anything.(10) )) == :(getMember(anything,10))
+end
+
+
+@test macroexpand( :( @dotted "weird".stuff )) == :(getMember("weird",:stuff))
+@test macroexpand( :( @dotted "weird"."stuff" )) == :(getMember("weird","stuff"))
+
+# basic usage
+
+DotOverload.getMember(t, m::Symbol) = begin
+  println("intercepted!")
+  return getfield(t, m)
+end
+
+@dotted function dontDoThatAtHome()
+  a = TT(42)
+  return a.F
+end
+
+@test 42 == dontDoThatAtHome() #result: 42, prints: "intercepted"
+
+# code equality
+
+DotOverload.getMember(t, m::Symbol) = Base.getfield(t, m)
+DotOverload.setMember!(t, m::Symbol, v) = Base.setfield!(t,m,v)
+
+function test1()
+  a = TT(3)
+  return a.F
+end
+
+@dotted function test2()
+  a = TT(3)
+  return a.F
+end
+
+@test @code_lowered(test1()) != @code_lowered(test2())
+@test @code_llvm(test1()) == @code_llvm(test2())
+
+# lua table like Dict
+
+@dotted function test3()
+  getMember(t::Dict, k) = t[k]
+  setMember!(t::Dict, k::Symbol, v) = t[k] = v
+
+  d = Dict()
+  d.key1 = 1
+  d.key2 = 2
+
+  return d.key1 + d.key2
+end
+
+@test test3() == 3
+
+
+# more examples
+
+@dotted function test4()
+  function getMember(t, k)
+    return getfield(t, k)
+  end
+  function setMember!(t, m, v)
+    return Base.setfield!(t,m,v)
+  end
+  function getMember(t::Dict, k)
+    v = nothing
+    try
+      v = t[k]
+      try
+        v = round(v*2)
+      catch
+      end
+    catch
+      t[k] = Dict()
+      v = t[k]
+    end
+    return v
+  end
+  function setMember!(t::Dict, k::Symbol, v)
+    t[k] = v*2
+  end
+
+  a = 10.5
+  b = Dict()
+
+  b.this.is.kinda.kool = a
+
+  return b.this.is.kinda.kool
+end
+
+@test test4() == 42
+
+# a module!
+
+@dotted module m
+
+using DotOverload
+DotOverload.getMember(t::Dict, k::Symbol) = t[k]
+DotOverload.setMember!(t::Dict, k::Symbol, v) = t[k] = v
+
+a = Dict()
+a[:stuff] = 25
+a.stuff *= 2
+
+foo() = a.stuff * 2
+
+end
+
+@test m.foo() == 100
+
+# REPL example (0.4):
+#
+# julia> using DotOverload
+#
+# julia> v = @dotted function() a.b = 32; a.b end
+# (anonymous function)
+#
+# julia> a = Dict()
+# Dict{Any,Any} with 0 entries
+#
+# julia> v()
+# ERROR: type Dict has no field b
+#  in setMember! at C:\SN\prj\julia\DotOverload\src\DotOverload.jl:128
+#  in anonymous at none:1
+#
+# julia> DotOverload.getMember(t::Dict, k) = t[k]
+# getMember (generic function with 2 methods)
+#
+# julia> DotOverload.setMember!(t::Dict, k, v) = t[k] = v
+# setMember! (generic function with 2 methods)
+#
+# julia> v()
+# 32
+#
+# julia>
